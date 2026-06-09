5c6fe4e99d145831d8fe199e8f248151b75c2071
butlerajamesab-lab <butlerajames.ab@gmail.com>
2026-06-08 21:11:00 -0700

    feat: add recognition pathway stage model

---
+++ client/src/types/recognition_pathway_stage.ts
@@ -0,0 +1,8 @@
+export type recognition_pathway_stage =
+  | "no_state_path"
+  | "no_federal_petition"
+  | "petition_filed"
+  | "active_review"
+  | "inactive_no_decision"
+  | "denied"
+  | "recognized";
