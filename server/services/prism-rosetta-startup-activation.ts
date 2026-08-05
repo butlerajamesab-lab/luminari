7d1085f1ba81f0c63e4d8053a00042dcad140785
butlerajamesab-lab <butlerajames.ab@gmail.com>
2026-08-05 14:01:45 -0700

    Start legislative-version decomposition with Rosetta and Prism queues

--- server/services/prism-rosetta-startup-activation.ts
+++ server/services/prism-rosetta-startup-activation.ts
@@ -1,3 +1,4 @@
+import { start_legislative_version_queue_worker } from "../civic-genome-legislative-version-queue-worker";
 import { run_rosetta_generation_activation_from_environment } from "../civic-genome-rosetta-generation-activation";
 import { start_rosetta_generation_activation_queue_worker } from "../civic-genome-rosetta-generation-queue-worker";
 import { activate_prism_for_rosetta_assembly } from "./prism-rosetta-activation";
@@ -6,6 +7,7 @@ import { start_prism_rosetta_queue_worker } from "./prism-rosetta-queue-worker";
 export async function run_prism_rosetta_activation_from_environment(): Promise<void> {
   start_prism_rosetta_queue_worker();
   start_rosetta_generation_activation_queue_worker();
+  start_legislative_version_queue_worker();
 
   // An explicitly configured Rosetta generation is assembled first. The queue
   // workers are already running, so the resulting completed assembly is claimed
