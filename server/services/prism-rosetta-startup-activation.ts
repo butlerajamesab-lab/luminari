a39777469c072aa2250deacf897391a5489aff77
butlerajamesab-lab <butlerajames.ab@gmail.com>
2026-08-06 11:26:36 -0700

    Start Docket jurisdiction activation worker

--- server/services/prism-rosetta-startup-activation.ts
+++ server/services/prism-rosetta-startup-activation.ts
@@ -1,6 +1,7 @@
 import { start_legislative_version_queue_worker } from "../civic-genome-legislative-version-queue-worker";
 import { run_rosetta_generation_activation_from_environment } from "../civic-genome-rosetta-generation-activation";
 import { start_rosetta_generation_activation_queue_worker } from "../civic-genome-rosetta-generation-queue-worker";
+import { start_docket_bill_activation_queue_worker } from "../docket-jurisdiction-activation-queue-worker";
 import { activate_prism_for_rosetta_assembly } from "./prism-rosetta-activation";
 import { start_prism_rosetta_queue_worker } from "./prism-rosetta-queue-worker";
 
@@ -8,6 +9,7 @@ export async function run_prism_rosetta_activation_from_environment(): Promise<v
   start_prism_rosetta_queue_worker();
   start_rosetta_generation_activation_queue_worker();
   start_legislative_version_queue_worker();
+  start_docket_bill_activation_queue_worker();
 
   // An explicitly configured Rosetta generation is assembled first. The queue
   // workers are already running, so the resulting completed assembly is claimed
