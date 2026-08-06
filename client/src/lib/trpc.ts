5aacc705153583c0cf9506e4c70e57d394bedf7f
butlerajamesab-lab <butlerajames.ab@gmail.com>
2026-08-06 05:15:47 -0700

    Install protected REST authentication at client bootstrap

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,7 @@
 import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../../../server/routers";
+import { installProtectedRestAuthTransport } from "@/lib/protected-rest-auth";
+
+installProtectedRestAuthTransport();
 
 export const trpc = createTRPCReact<AppRouter>();
