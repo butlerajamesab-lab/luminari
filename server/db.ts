bbae5c16bab16d6e433f74c6aca6de5961e89483
butlerajamesab-lab <butlerajames.ab@gmail.com>
2026-08-08 12:03:21 -0700

    Use canonical chronology in case timeline consumers

--- server/db.ts
+++ server/db.ts
@@ -10,11 +10,11 @@
 export * from "./db-legacy";
 export {
   createCase,
-  getCaseTimelineData,
   getCaseNarrative,
   upsertCaseNarrative,
 } from "./case-contract-compat";
 export { getCaseStats } from "./case-stats-intake-compat";
+export { getCaseTimelineData } from "./case-timeline-intake-compat";
 export {
   getSnapshot,
   getOpenSnapshot,
